7717f9787eef8643fe1eeca0b1b805291f3add0e
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-05-14 12:44:25 +0530

    added test for reverse function

--- test/test.c
+++ test/test.c
@@ -340,6 +340,44 @@ void test_remove_at()
   free(num6);
 }
 
+void test_reverse()
+{
+  printf("-------reverse list------\n");
+  char *message = NULL;
+  List_ptr list = create_list();
+  int result;
+
+  message = "should reverse if the list is empty";
+  int expected1[] = {};
+  List_ptr reversed1 = reverse(list);
+  result = assert(list, expected1, 0) && assert(reversed1, expected1, 0);
+  print_status(result, message);
+  free_list(reversed1);
+
+  int *num1 = malloc(sizeof(int));
+  *num1 = 1;
+  add_to_list(list, num1);
+  int *num2 = malloc(sizeof(int));
+  *num2 = 2;
+  add_to_list(list, num2);
+  int *num3 = malloc(sizeof(int));
+  *num3 = 3;
+  add_to_list(list, num3);
+
+  message = "should reverse a list containing large elements";
+  int reverse_expected[] = {3, 2, 1};
+  int original_list[] = {1, 2, 3};
+  List_ptr reversed2 = reverse(list);
+  result = assert(list, original_list, 3) && assert(reversed2, reverse_expected, 3);
+  print_status(result, message);
+  free_list(reversed2);
+
+  free_list(list);
+  free(num1);
+  free(num2);
+  free(num3);
+}
+
 int main(void)
 {
   test_add_to_list();
@@ -349,5 +387,6 @@ int main(void)
   test_remove_from_end();
   test_remove_at();
   test_clear_list();
+  test_reverse();
   return 0;
 }
